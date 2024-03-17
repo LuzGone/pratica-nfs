91ac39fd24e46e2cce3127c96f68b37dd3e59f84
Luiz Gonzaga <luizg.academico@gmail.com>
2024-03-17 13:47:19 -0300

    Making the server listing files for client

--- Pratica-NFS/src/Server.java
+++ Pratica-NFS/src/Server.java
@@ -3,10 +3,17 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Server {
     public static void main(String[] args) throws IOException{
-        System.out.println("== Cliente ==");
+        Path path = Paths.get("Z:\\IFPB\\Sistemas-Distribuidos\\Pratica-NFS\\Pratica-NFS\\arquivos");
+
+        System.out.println("== Server ==");
 
         ServerSocket serverSocket = new ServerSocket(7001);
         Socket socket = serverSocket.accept();
@@ -14,14 +21,26 @@ public class Server {
         DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 
         DataInputStream dis = new DataInputStream(socket.getInputStream());
+        
+        System.out.println("Client - " + socket.getInetAddress());
 
         while (true){
-            System.out.println("Cliente - " + socket.getInetAddress());
-
-            String mensagem = dis.readUTF();
-            System.out.println("-> " + mensagem);
-
-            dos.writeUTF("Recebi sua mensagem: " + mensagem);
+            String message = dis.readUTF();
+            System.out.println("-> " + message);
+            if(message.equals("readdir")){
+                String saida = "Those are the files...\n";
+                try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+                    for (Path file: stream) {
+                        saida += (file.getFileName().toString()+"\n");
+                    }
+                    dos.writeUTF(saida);
+                    System.out.println(saida);
+                } catch (IOException e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            }else{
+                dos.writeUTF("Receive your message - " + message);
+            }
         }
     }
 }
